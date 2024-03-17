50df493f7cd9d01d38b888d8e97c2b3c0fea945f
Antonis Papaioannou <mrantonp@gmail.com>
2024-03-17 13:58:36 +0200

    complete teacher mode and courses

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -3,7 +3,7 @@ import SidebarRoutes from "./sidebar-routes";
 
 const Sidebar = () => {
   return ( 
-    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm" >
+    <div className="h-full border-r flex flex-col overflow-y-auto bg-gray-100 shadow-sm" >
       <div className="p-6">
         <Logo />
       </div>
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,13 +1,37 @@
-"use client";
+'use client';
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton } from '@clerk/nextjs';
+import { usePathname } from 'next/navigation';
+import { Button } from './ui/button';
+import { LogOut } from 'lucide-react';
+import Link from 'next/link';
 
 const NavbarRoutes = () => {
-  return ( 
+  const pathname = usePathname();
+  
+
+  const isTeacherPage = pathname?.startsWith('/teacher');
+  const isPlayerPage = pathname?.includes('/chapter');
+
+  return (
     <div className="flex gap-x-2 ml-auto">
-      <UserButton />
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button size="sm" variant="default">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="default">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
-   );
-}
- 
+  );
+};
+
 export default NavbarRoutes;
