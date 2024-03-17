71c2ba3dcc2b526811e6ed70d14c034f766559ca
Antonis Papaioannou <mrantonp@gmail.com>
2024-03-17 19:33:21 +0200

    create course page

---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,96 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  FormField,
+  FormItem,
+  Form,
+  FormLabel,
+  FormControl,
+  FormDescription,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  title: z.string().min(1, { message: 'Title must be at least 1 character' }),
+});
+
+const CreatePage = () => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: '',
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const response = await axios.post('/api/course', values);
+      router.push(`/teacher/courses/${response.data.id}`);
+    } catch  {
+      toast.error('Failed to create course');
+    }
+  };
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+      <div className="">
+        <h1 className="text-4xl font-bold">Name your course</h1>
+        <p className="text-sm text-slate-700">
+          What would you name your course? You can change this later.
+        </p>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 mt-10"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className='text-2xl font-bold'>Course title</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. Introduction to Web Development"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    What will you teach in this course?
+                  </FormDescription>
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href="/">
+                <Button type="button" variant="destructive">
+                  Cancel
+                </Button>
+              </Link>
+              <Button 
+                type='submit'
+                disabled={!isValid || isSubmitting}
+              >
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePage;
